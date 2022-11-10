7570d2fadd59a993ecb07455f86db4b2ed7550be
Estudiante <Estudiante@DESKTOP-GUU2CNQ>
2022-11-10 18:23:10 -0500

    Agregar archivos de proyecto.

---
+++ CapaPresentacion/ServicioNormal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDatos;
+
+namespace CapaPresentacion
+{
+    public partial class ServicioNormal : Form
+    {
+        private CDatosC cdatos = new CDatosC();
+        public ServicioNormal()
+        {
+            InitializeComponent();
+        }
+
+        private void guardarNormal()
+        {
+            string linea;
+            linea = txtIdentificacion1.Text + "" + txtNombre1.Text + "" + txtApellido1.Text;
+
+
+            if (cdatos.escribir1(linea) == true)
+
+                MessageBox.Show("Ha guardado un cliente casual");
+
+            else
+                MessageBox.Show("No se ha podido guardar el cliente...");
+        }
+
+        private void btnGuardar1_Click(object sender, EventArgs e)
+        {
+            guardarNormal();
+        }
+    }
+}
